4ea92fdf0ac42afd422066deccd329516904bace
myprivateproject <myprivateproject@outlook.com>
2020-11-10 08:42:52 +0530

    lifecycle scope

--- app/src/main/java/com/example/androidcoroutines/MainActivity.kt
+++ app/src/main/java/com/example/androidcoroutines/MainActivity.kt
@@ -2,11 +2,9 @@ package com.example.androidcoroutines
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.renderscript.ScriptGroup
 import android.util.Log
-import android.widget.Toast
+import androidx.lifecycle.lifecycleScope
 import com.example.androidcoroutines.databinding.ActivityMainBinding
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
@@ -14,35 +12,32 @@ class MainActivity : AppCompatActivity() {
 
     private val TAG = "MainActivity"
     lateinit var binding: ActivityMainBinding
-    private var count = 0
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.download.setOnClickListener { view ->
-            // If  you not launch with coroutine then download() is running on main/ui thread  so may be ui is lagging
-            CoroutineScope(Dispatchers.IO).launch {
-                download()
-            }
 
+        lifecycleScope.launch(Dispatchers.IO) {
+            Log.e(TAG, "onActivityCreated: ${Thread.currentThread().name}")
         }
 
-        binding.add.setOnClickListener { view ->
-            binding.textView.text = count++.toString()
+        lifecycleScope.launch(Dispatchers.IO) {
+            Log.e(TAG, "onActivityCreated: ${Thread.currentThread().name}")
         }
 
+        lifecycleScope.launchWhenCreated {
+            Log.e(TAG, "onActivityCreated2: ${Thread.currentThread().name}")
+        }
+        lifecycleScope.launchWhenStarted {
+            Log.e(TAG, "onActivityCreated3: ${Thread.currentThread().name}")
+        }
+
+        lifecycleScope.launchWhenResumed {
+            Log.e(TAG, "onActivityCreated4: ${Thread.currentThread().name}")
+        }
 
     }
-
-
-    private fun download() {
-        for (i in 1..200000) {
-            Log.e(TAG, "Count is $i in ${Thread.currentThread().name}")
-        }
-    }
-
 
 }
